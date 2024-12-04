ed49ed6e1c88696bd6d251bc106b1e0932d8f14c
JB <jb@bms-engineering.lu>
2024-12-04 14:18:16 +0100

    Ajoutez des fichiers projet.

---
+++ BlazorAppPwaAssetsIntegrityFails/BlazorAppPwaAssetsIntegrityFails/Program.cs
@@ -0,0 +1,34 @@
+using BlazorAppPwaAssetsIntegrityFails.Client.Pages;
+using BlazorAppPwaAssetsIntegrityFails.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorAppPwaAssetsIntegrityFails.Client._Imports).Assembly);
+
+app.Run();
